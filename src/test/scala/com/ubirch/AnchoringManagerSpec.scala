4fa656335e838982de1df496dab976808e671d2d
Carlos Sanchez <caaugustoss@gmail.com>
2020-09-24 17:02:55 +0200

    UNG-442 Adding body for TDD

---
+++ src/test/scala/com/ubirch/AnchoringManagerSpec.scala
@@ -0,0 +1,11 @@
+package com.ubirch
+
+class AnchoringManagerSpec extends TestBase {
+
+  "The Anchoring Manager " must {
+    "expected behavior 1" in { fail("Not implemented ") }
+    "expected behavior 2" in { fail("Not implemented ") }
+    "expected behavior 3" in { fail("Not implemented ") }
+  }
+
+}
